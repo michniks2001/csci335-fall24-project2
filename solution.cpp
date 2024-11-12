8fd7df93f42ffd7893d98775fa40ee248caa29fc
Sam Michnik <michniksamuel@gmail.com>
2024-11-12 14:31:50 -0500

    implemented query() and began FileTrie implementation

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,23 @@
 #include "FileAVL.hpp"
 #include "File.hpp"
+#include "FileTrie.hpp"
+
+
+// Helper function that implements an in-order traversal of the tree to collect thge files within the given range
+inline void collectFiles(Node* node, size_t min, size_t max, std::vector<File*>& result) {
+    if (node == nullptr) return;
+
+    collectFiles(node->left_, min, max, result);
+
+    for (auto file : node->files_) {
+        if (file->getSize() >= min && file->getSize() <= max) {
+            result.push_back(file);
+        }
+    }
+
+    collectFiles(node->right_, min, max, result);
+
+}
 
 // ALL YOUR CODE SHOULD BE IN THIS FILE. NO MODIFICATIONS SHOULD BE MADE TO FILEAVL / FILE CLASSES
 // You are permitted to make helper functions (and most likely will need to)
@@ -18,7 +36,34 @@
 std::vector<File*> FileAVL::query(size_t min, size_t max) {
     std::vector<File*> result;
 
-    // Your code here.
+    if (min >= max) std::swap(min, max);
+
+    collectFiles(root_, min, max, result);
 
     return result;
+}
+
+// FileTrie Implementation
+
+
+
+
+// Constructor
+FileTrie::FileTrie() : head(new FileTrieNode('\0')) {}
+
+
+// addFile
+void FileTrie::addFile(File* f) {
+    std::string filename = f->getName();
+    std::transform(filename.begin(), filename.end(), filename.begin(), std::tolower<char>);
+    FileTrieNode* current = head;
+
+    for (char ch : filename) {
+        if (current->next.find(ch) == current->next.end()) {
+            current->next[ch] = new FileTrieNode(ch);
+        }
+
+        current = current->next[ch];
+        current->matching.insert(f);
+    }
 }
